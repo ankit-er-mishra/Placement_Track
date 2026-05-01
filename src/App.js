3b6c94a9790c872197ab95737e789cb7b34aec6e
ankit-er-mishra <ankit.er.mishra@gmail.com>
2026-05-01 23:55:48 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [userName, setUserName] = useState('');
+  const [data, setData] = useState(null);
+
+  const handleSave = () => {
+    localStorage.setItem("leetcodeUser", userName);
+    alert("Username saved!");
+  };
+
+  const fetchData = (user) => {
+    fetch(`https://alfa-leetcode-api.onrender.com/${user}`)
+      .then(res => res.json())
+      .then(res => {
+        console.log("API DATA:", res);
+        setData(res);
+      })
+      .catch(err => console.error(err));
+  };
+
+  useEffect(() => {
+    const saved = localStorage.getItem("leetcodeUser");
+    if (saved) {
+      setUserName(saved);
+      fetchData(saved);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '20px' }}>
+      <h1>NextStep</h1>
+
+      <input
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+        placeholder="Enter LeetCode username"
+      />
+
+      <button onClick={handleSave}>
+        Save
+      </button>
     </div>
   );
 }
